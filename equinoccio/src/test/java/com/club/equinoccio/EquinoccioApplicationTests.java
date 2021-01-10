f02ffbacfdd63c804182a0f8d973d1bb4c5fdd9b
benjvtdh <cortesbenja@gmail.com>
2021-01-10 12:27:12 -0300

    aplicacion web

---
+++ equinoccio/src/test/java/com/club/equinoccio/EquinoccioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.equinoccio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquinoccioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
