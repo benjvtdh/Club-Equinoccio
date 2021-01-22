1baf6f05a30104a8b1740e6d74090de322f2876b
benjv_tdh <cortesbenja@gmail.com>
2021-01-22 10:39:14 -0300

    Agregando form y salidas a templates

--- equinoccio/src/main/java/com/club/equinoccio/controladores/InicioControlador.java
+++ equinoccio/src/main/java/com/club/equinoccio/controladores/InicioControlador.java
@@ -5,6 +5,8 @@ import com.club.equinoccio.servicios.UsuarioServicio;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,19 +36,25 @@ public class InicioControlador {
         return mv;
     }
     
-//    @GetMapping("/index")
-//    public String recuperar(Authentication auth, HttpSession session){
-//        String username = auth.getName();
+    @GetMapping("/index")
+    public String recuperar(Authentication auth, HttpSession session){
+        String username = auth.getName();
+        System.out.println("Nombre del usuario: " + username);
+        for (GrantedAuthority rol : auth.getAuthorities()) {
+            System.out.println("ROL: " + rol.getAuthority());
+        }
+        
 //        if(session.getAttribute("usuario") == null ){
-//            Usuario usuario = usuarioServicio.buscarPorNombre(username);
+//            UserDetails usuario = usuarioServicio.loadUserByUsername(username);
+//            usuario.g
 //            usuario.setPassword(null);
 //            System.out.println(usuario);
 //            session.setAttribute("usuario", usuario);
 //            
 //        }
-//        return "redirect:/";
-//        
-//        
-//        
-//    }
+        return "redirect:/";
+        
+        
+        
+    }
 }
