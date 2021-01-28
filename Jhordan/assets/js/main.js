9de49ca71a0e191c47364b4c310af4bcdccc1985
Jhoran-Donoso <jhordan.donoso.parra@gmail.com>
2021-01-28 10:40:06 -0300

    single salida

--- Jhordan/assets/js/main.js
+++ Jhordan/assets/js/main.js
@@ -157,7 +157,7 @@
 
   // Testimonials carousel (uses the Owl Carousel library)
    $(".testimonials-carousel").owlCarousel({
-    autoplay: false,
+    autoplay: true,
     dots: true,
     loop: true,
     responsive: {
